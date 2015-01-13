41b3552eada5072625566aaef927327427dda089
Pim Rutgers <pim.rutgers@gmail.com>
2015-01-13 10:54:42 +0100

    Init magic. hook inside class

--- __init__.py
+++ __init__.py
@@ -15,11 +15,21 @@ __plugin_name__ = "ActionTriggerPlugin"
 __plugin_version__ = "0.1"
 __plugin_description__ = "Hooks to specific serial commands from the printer. Actions are handled accordingly"
 
+def __plugin_init__():
+        global _plugin
+        global __plugin_implementations__
+        global __plugin_hooks__
+
+        _plugin = ActionTriggerPlugin()
+        __plugin_implementations__ = [_plugin]
+        __plugin_hooks__ = {'octoprint.comm.protocol.action': _plugin.hook_actiontrigger}
+
 class ActionTriggerPlugin(octoprint.plugin.TemplatePlugin,
                           octoprint.plugin.AssetPlugin):
         def __init__(self):
                 self._logger = logging.getLogger("octoprint.plugin.actiontrigger")
                 self._pluging_manager = None
+
 
         @property
         def plugin_manager(self):
@@ -44,16 +54,11 @@ class ActionTriggerPlugin(octoprint.plugin.TemplatePlugin,
     		}
 
         ##~~ ActionTriggerPlugin
+        def hook_actiontrigger(self, comm_obj, line, action_trigger):
+                if action_trigger == "door":
+                        self._send_client_message(action_trigger, line)
 
 
         # Send trigger to front end
     	def _send_client_message(self, message_type, data=None):
 		      self.plugin_manager.send_plugin_message("actiontrigger", dict(type=message_type, data=data))
-
-
-def hook_actiontrigger(comm_obj, line, action_trigger):
-        if action_trigger == "door":
-                self._send_client_message(self, action_trigger, line)
-
-__plugin_hooks__ = {'octoprint.comm.protocol.action': hook_actiontrigger}
-__plugin_implementations__ = [ActionTriggerPlugin()]
